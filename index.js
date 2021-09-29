6acf3c2854cca2066e070f8bbd5f4a9647141a95
David <trey3@hotmail.com>
2021-09-29 15:47:31 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const ppl = ["Lisa", "Kaitlin", "Jan"]
+const event = ["surprise"]
+
+function writeCards(ppl, event) {
+    let tycards = [];
+    for (let i = 0; i < ppl.length; i++) {
+        tycards.push(`Thank you, ${ppl[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return tycards;
+}
+
+function countDown(num) {
+    for (let num = 10; num >= 0; num--) {
+        console.log(num)
+    }
+}
